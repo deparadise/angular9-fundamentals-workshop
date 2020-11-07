42081c124760a10595395adfd053af19d1ee2cd4
David Pineda <david.pineda@deparadise.com>
2020-11-06 22:11:01 -0800

    Example work for lesson: Components > Property Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,16 @@ import { MaterialModule } from './material.module';
 import { HomeComponent } from './home/home.component';
 import { CoursesComponent } from './courses/courses.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+	 HttpClientModule,
+	 FormsModule
   ],
   declarations: [
     AppComponent,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title = "HELLO DERP";
+
+	themeColor = 'red';
+
   courseLessons = [
     { title: 'Hello Angular' },
     { title: 'Component Fundamentals' },
