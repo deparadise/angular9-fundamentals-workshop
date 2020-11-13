5a0125d3f1892ff5709971aeb72fdfdd5052a44f
David Pineda <david.pineda@deparadise.com>
2020-11-12 23:49:29 -0800

    LAZY LOADING THE LOGIN MODULE!!!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'courses', component: CoursesComponent },
-  { path: '**', redirectTo: '/home' }
+	{ path: 'home', component: HomeComponent },
+	{ path: 'courses', component: CoursesComponent },
+	{
+		path: 'login',
+		loadChildren: () =>
+			import('./login/login.module').then((m) => m.LoginModule),
+	},
+	{ path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
